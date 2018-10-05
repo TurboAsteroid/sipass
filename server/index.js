7b83750c0f3ac760112280e2079ecd8a8e2f3210
nightwelf <bpo@elem.ru>
2018-10-05 10:05:18 +0500

    работает для теста

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 'use strict'
 const http = require('http')
-const express = require('express')
-const app8686 = express()
-const httpServer8686 = http.createServer(app8686)
-httpServer8686.listen(8686, () => {
+const app8686 = require('./app')
+const server = http.createServer(app8686)
+server.listen(8686, () => {
   console.log('HTTP Server running on port 8686')
 })
