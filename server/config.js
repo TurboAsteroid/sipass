458ba98ac9c4ed7405a4014a2dc28d1d17fe0d02
Vasily Lobarev <vlobarev@gmail.com>
2018-10-05 09:58:06 +0500

    что-то пока не работает

---
+++ server/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  /* 'dbHost' : 'localhost',
+    'dbDatabase' : 'alertnotification',
+    'dbUser': 'alnot',
+    'dbPassword': 'df734##12iiRR2==-', */
+/*
+    'dbHost' : '10.1.255.117',
+    'dbDatabase' : 'alertnotification',
+    'dbUser': 'nightwelf',
+    'dbPassword': 'kUnKu11/caNKeRunk',
+*/
+  'ldapurl': 'ldap://elem-dc0.elem.ru',
+  'ldapbaseDN': 'dc=elem,dc=ru',
+  'username': 'gs2@elem.ru',
+  'password': 'gs2-1',
+  'jwtSecret': 'iasnmdflkiudj9)*&89079q234rhw^(912683764-907HHHuashdfoiuaostd'
+}
