3bc41661cfa381babd4321630fe7c11ec3d194d1
nightwelf <bpo@elem.ru>
2018-10-12 17:31:35 +0500

    не работает сука!

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,16 +1,38 @@
 <template>
   <v-text-field
+    v-model="propusk"
     style="max-width: 500px;"
     solo-inverted
     flat
     label="Поиск пропуска"
-    prepend-icon="search"
-  ></v-text-field>
+    prepend-icon="search">
+  </v-text-field>
 </template>
 
 <script>
+// 0021528669
 export default {
-  name: 'search'
+  name: 'search',
+  props: ['propusk'],
+  methods: {
+    clear () {}
+  },
+  watch: {
+    propusk: function (val, oldVal) {
+      console.log(val)
+      console.log(this.propusk)
+      if (val.toString().length === 10) {
+        this.$emit('clear_propusk')
+        this.$router.push({
+          name: 'card',
+          params: {kpp: getRandomInt(-10000000, -2), doknr: getRandomInt(-10000000, -2), propusk: val}
+        })
+      }
+    }
+  }
+}
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import 'es6-promise/auto'
 import Vuex from 'vuex'
 import navigation from './navigation'
+import search from './search'
 
 Vue.use(Vuex)
 const store = new Vuex.Store({
-  modules: {navigation}
+  modules: {navigation, search}
 })
 export default store
