68b4e4a77994c04444cf96060e09ffad597621ba
Vasily Lobarev <vlobarev@gmail.com>
2018-10-24 13:44:57 +0500

    поправлена внешность крточки. small fixes

---
+++ src/components/approve_users_list.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-content>
+      <div slot="header"><h3>Информация об отметке согласующими</h3></div>
+      <v-card>
+        <v-card-text>
+          <v-data-table :headers="headers"  :items="items.APPRDATA" hide-actions>
+            <template slot="items" slot-scope="props">
+              <tr class="nohover">
+                <td class="text-xs-left">{{ props.item.APRST }}</td>
+                <td class="text-xs-left">{{ props.item.APRNAME_FULL }}</td>
+                <td class="text-xs-left">{{ props.item.CREATED_BY_NAME }}</td>
+                <td class="text-xs-left">{{ M(props.item.CREATED_ON + props.item.CREATED_TM, 'YYYYMMDDHHmmSS').format('DD.MM.YYYY HH:mm:SS') }}</td>
+              </tr>
+            </template>
+          </v-data-table>
+        </v-card-text>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: 'approve_users_list',
+  props: ['items', 'headers'],
+  data () {
+    return {
+      M: moment
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/provide_a_pass.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-layout wrap align-start justify-start row fill-height>
+      <v-flex xs12 md6 d-flex>
+        <v-text-field
+          label="Кликните сюда и считайте карту"
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs6 md3 d-flex>
+        <v-btn @click="" color="info"><i class="material-icons">check_circle</i>&nbsp;Отметить вход</v-btn>
+      </v-flex>
+      <v-flex xs6 md3 d-flex>
+        <v-btn @click="" color="info"><i class="material-icons">redo</i>&nbsp;На доработку</v-btn>
+      </v-flex>
+    </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'provide_a_pass'
+}
+</script>
+
+<style scoped>
+
+</style>
