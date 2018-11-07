02729bff2cef2fb4e816df499dceaf2630769178
nightwelf <bpo@elem.ru>
2018-11-07 11:23:29 +0500

    server fixed (to https) GS3-14 GS3-13

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 'use strict'
-const http = require('http')
 const app8686 = require('./app')
-const server = http.createServer(app8686)
+const fs = require('fs')
+const https = require('https')
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/apps.elem.ru/privkey.pem', 'utf8')
+const certificate = fs.readFileSync('/etc/letsencrypt/live/apps.elem.ru/cert.pem', 'utf8')
+const ca = fs.readFileSync('/etc/letsencrypt/live/apps.elem.ru/chain.pem', 'utf8')
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+}
+const server = https.createServer(credentials, app8686)
 server.listen(8686, () => {
   console.log('HTTP Server running on port 8686')
 })
