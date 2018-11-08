2e7d049e0c11f66f49270f83779e0e1b35b68829
Vasily Lobarev <vlobarev@gmail.com>
2018-11-08 10:58:31 +0500

    '${req.connection.remoteAddress}'

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,10 @@
 'use strict'
 const mysql = require('mysql2/promise')
 module.exports = function (app, config, router) {
-  async function logger (user, message, url) {
+  async function logger (user, message, url, req) {
     try {
       const connection = await mysql.createConnection(config.mariadb)
-      await connection.execute(`INSERT INTO gs3.logs_auth(\`user\`, message, url) VALUES('${user}', '${message}', '${url}');`)
+      await connection.execute(`INSERT INTO gs3.logs_auth(\`user\`, message, url, ip) VALUES('${user}', '${message}', '${url}', '${req.connection.remoteAddress}');`)
       await connection.end()
     } catch (e) {
       console.error(e)
@@ -14,7 +14,7 @@ module.exports = function (app, config, router) {
     if (
       req.originalUrl === '/auth/login' || req.originalUrl === '/getallphotos'
     ) {
-      await logger('guest', 'guest', '/auth/login')
+      await logger('guest', 'guest', '/auth/login', req)
       next()
     } else if ((req.headers.authorization !== undefined && req.headers.authorization !== null) || (req.query.jwt !== undefined && req.query.jwt !== null)) {
       try {
@@ -25,33 +25,33 @@ module.exports = function (app, config, router) {
           token = req.headers.authorization.replace(/Bearer /g, '')
         }
         const decoded = jwt.verify(token, config.jwtSecret)
-        await logger(`${decoded.login}`, 'try login', req.originalUrl)
+        await logger(`${decoded.login}`, 'try login', req.originalUrl, req)
         ad.findUser(decoded.login, async function (err, user) {
           if (err) {
             console.log('ERROR: ' + JSON.stringify(err))
             res.status(403).send(JSON.stringify(err))
-            await logger(`${decoded.login}`, 'login failed: server error', req.originalUrl)
+            await logger(`${decoded.login}`, 'login failed: server error', req.originalUrl, req)
             return
           }
           if (!user) {
             console.log(`User: ${decoded.login} not found.`)
-            await logger(`${decoded.login}`, 'user not found', req.originalUrl)
+            await logger(`${decoded.login}`, 'user not found', req.originalUrl, req)
           } else {
             console.log(JSON.stringify(user))
             ad.authenticate(decoded.login, decoded.password, async function (err, auth) {
               if (err) {
                 console.log('ERROR: ' + JSON.stringify(err))
                 res.status(403).send(JSON.stringify(err))
-                await logger(`${decoded.login}`, 'password wrong', req.originalUrl)
+                await logger(`${decoded.login}`, 'password wrong', req.originalUrl, req)
                 return
               }
               if (auth) {
-                await logger(`${decoded.login}`, 'authenticated', req.originalUrl)
+                await logger(`${decoded.login}`, 'authenticated', req.originalUrl, req)
                 console.log(`${decoded.login} Authenticated!`)
                 next()
               } else {
                 console.log(`${req.body.user} Authenticated failed!`)
-                await logger(`${user}`, 'authenticated failed', req.originalUrl)
+                await logger(`${user}`, 'authenticated failed', req.originalUrl, req)
                 res.status(403).send(JSON.stringify(err))
               }
             })
@@ -74,23 +74,23 @@ module.exports = function (app, config, router) {
       if (err) {
         console.log('ERROR: ' + JSON.stringify(err))
         res.status(403).send(JSON.stringify(err))
-        await logger(`${req.body.user.login}`, err.message, req.originalUrl)
+        await logger(`${req.body.user.login}`, err.message, req.originalUrl, req)
         return
       }
       if (!user) {
-        await logger(`${req.body.user.login}`, 'user not found', req.originalUrl)
+        await logger(`${req.body.user.login}`, 'user not found', req.originalUrl, req)
         console.log(`User: ${req.body.user.login} not found.`)
       } else {
         console.log(JSON.stringify(user))
         ad.authenticate(req.body.user.login, req.body.user.password, async function (err, auth) {
           if (err) {
-            await logger(`${req.body.user.login}`, err.message, req.originalUrl)
+            await logger(`${req.body.user.login}`, err.message, req.originalUrl, req)
             console.log('ERROR: ' + JSON.stringify(err))
             res.status(403).send(JSON.stringify(err))
             return
           }
           if (auth) {
-            await logger(`${req.body.user.login}`, 'authenticated', req.originalUrl)
+            await logger(`${req.body.user.login}`, 'authenticated', req.originalUrl, req)
             console.log(`${req.body.user.login} Authenticated!`)
             let token = await jwt.sign({login: req.body.user.login, password: req.body.user.password},
               config.jwtSecret,
@@ -101,7 +101,7 @@ module.exports = function (app, config, router) {
             console.log(token)
             console.log(jwt.verify(token, config.jwtSecret))
           } else {
-            await logger(`${req.body.user.login}`, 'authenticated failed', req.originalUrl)
+            await logger(`${req.body.user.login}`, 'authenticated failed', req.originalUrl, req)
             console.log(`${req.body.user} Authenticated failed!`)
             res.status(403).send(JSON.stringify(err))
           }
