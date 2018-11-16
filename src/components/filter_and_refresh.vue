f534315d501d799e38d25c1e6efabf3efcd95604
Vasily Lobarev <vlobarev@gmail.com>
2018-11-16 11:15:10 +0500

    GS3-44 GS3-35 small fixes

--- src/components/filter_and_refresh.vue
+++ src/components/filter_and_refresh.vue
@@ -1,10 +1,13 @@
 <template>
     <v-layout wrap align-center fluid grid-list-xl>
-      <v-flex xs10 d-flex>
+      <v-flex xs6 d-flex>
         <v-select :items="kpps" label="Контрольно-пропускной пункт" v-model="kpp"/>
       </v-flex>
+      <v-flex xs4 d-flex>
+        <v-select :items="when" label="Начало действия пропуска" v-model="whenDate"/>
+      </v-flex>
       <v-flex xs2 d-flex>
-        <v-btn @click="$emit('refresh', kpp)" color="info" flat><i class="material-icons">refresh</i> Обновить</v-btn>
+        <v-btn @click="$emit('refresh')" color="info" flat><i class="material-icons">refresh</i> Обновить</v-btn>
       </v-flex>
     </v-layout>
 </template>
@@ -15,12 +18,31 @@ export default {
   props: ['kpps'],
   data () {
     return {
-      kpp: 'Все'
+      when: [
+        {text: 'Сегодня', value: 'today'},
+        {text: 'Завтра', value: 'tomorrow'},
+        {text: 'Все', value: 'all'}
+      ]
     }
   },
-  watch: {
-    kpp (val, oldVal) {
-      this.$emit('kpp_filter_changed', val)
+  computed: {
+    kpp: {
+      get: function () {
+        return this.$store.getters['listFilter/getKpp']
+      },
+      set: function (val) {
+        let toCommit = this.kpps.filter(it => it.value === val)
+        this.$store.commit('listFilter/changeKpp', toCommit[0])
+      }
+    },
+    whenDate: {
+      get: function () {
+        return this.$store.getters['listFilter/getWhenDate']
+      },
+      set: function (val) {
+        let toCommit = this.when.filter(it => it.value === val)
+        this.$store.commit('listFilter/changeWhen', toCommit[0])
+      }
     }
   }
 }
