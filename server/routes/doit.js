92faa1ef8c6bcc28a5cee2a4a8076e3570b36c1f
nightwelf <bpo@elem.ru>
2019-03-06 12:30:58 +0500

    Исправлен критический баг
    -
    
    METADATA: -

--- server/routes/doit.js
+++ server/routes/doit.js
@@ -5,13 +5,11 @@ const querystring = require('querystring')
 module.exports = function (app, config, router) {
   router.post('/doit', async function (req, res) {
     if (req.body.action === 'IN') {
-      await axios.get(`https://sap-prx.ugmk.com/ummc/permit/main?sap-user=${config.sap.u}&sap-password=${config.sap.p}&action=IN
-                        &doknr=${req.body.doknr}
-                        &ckeckpoint=${req.body.ckeckpoint}`)
+      const result = await axios.get(`https://sap-prx.ugmk.com/ummc/permit/main?sap-user=${config.sap.u}&sap-password=${config.sap.p}&action=IN&doknr=${req.body.doknr}&propusk=${req.body.cardNumber}&ckeckpoint=${req.body.ckeckpoint}`)
+      console.log(result)
     } else if (req.action === 'RET') {
-      await axios.post(`https://sap-prx.ugmk.com/ummc/permit/main?sap-user=${config.sap.u}&sap-password=${config.sap.p}&action=RET
-                        &doknr=${req.body.doknr}
-                        &ckeckpoint=${req.body.ckeckpoint}`, querystring.stringify({ notes: req.body.notes }))
+      const result = await axios.post(`https://sap-prx.ugmk.com/ummc/permit/main?sap-user=${config.sap.u}&sap-password=${config.sap.p}&action=RET&doknr=${req.body.doknr}&ckeckpoint=${req.body.ckeckpoint}`, querystring.stringify({ notes: req.body.notes }))
+      console.log(result)
     }
     res.sendStatus(200)
   })
