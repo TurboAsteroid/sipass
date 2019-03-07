978ce89755835aa5e13365fded5043a638d37bba
Vasily Lobarev <vlobarev@gmail.com>
2019-03-07 09:52:20 +0500

    fix
    -
    
    METADATA: -

--- src/components/provide_a_pass.vue
+++ src/components/provide_a_pass.vue
@@ -47,7 +47,7 @@
             <v-divider></v-divider>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="primary" @click="this.dialog = false">
+              <v-btn color="primary" @click="dialog = false">
                 OK
               </v-btn>
             </v-card-actions>
