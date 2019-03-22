dd24bd5660c3ceb86ee9f75eb5738afe5ad89108
nightwelf <bpo@elem.ru>
2019-03-22 15:05:57 +0500

    Очередная попытка устранить проблемы с отображением статусов
    -
    
    METADATA: v3.r14

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'es6-promise/auto'
 import auth from '@/auth'
 auth.initAuth()
 Vue.prototype.$appName = 'Разовый пропуск'
-Vue.prototype.$appVersion = 'v3.r14'
+Vue.prototype.$appVersion = 'v3.r15'
 try {
   Vue.prototype.$globalUserData = {permissions: {}, kpps: []}
   if (localStorage.getItem('globalUserData') !== 'undefined') {
