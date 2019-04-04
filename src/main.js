e01f7107ee178c56f7b37d6dc4b38f957b3e40a7
nightwelf <bpo@elem.ru>
2019-04-04 10:09:41 +0500

    Вместо номера сапа показывается номер карточки сипаса, если она есть
    -
    
    METADATA: v3.r16

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'es6-promise/auto'
 import auth from '@/auth'
 auth.initAuth()
 Vue.prototype.$appName = 'Разовый пропуск'
-Vue.prototype.$appVersion = 'v3.r15'
+Vue.prototype.$appVersion = 'v3.r16'
 try {
   Vue.prototype.$globalUserData = {permissions: {}, kpps: []}
   if (localStorage.getItem('globalUserData') !== 'undefined') {
