6e062bfd28af7ea69a8626682d969eacdfd151ae
nightwelf <bpo@elem.ru>
2019-09-04 15:03:36 +0500

    исправлены опечатки
    -
    
    METADATA: v3.r20

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -2,7 +2,7 @@
   <v-layout align-start justify-start column style="padding-top: 18px">
     <v-btn color="info" flat @click="approval" block><i class="material-icons">done</i>&nbsp;&nbsp;&nbsp;На согласовании</v-btn>
     <v-btn color="info" flat @click="approved" block><i class="material-icons">done_all</i>&nbsp;&nbsp;&nbsp;Согласованы</v-btn>
-    <v-btn color="info" flat @click="inside" block><i class="material-icons">location_on</i>&nbsp;&nbsp;&nbsp;На териитории</v-btn>
+    <v-btn color="info" flat @click="inside" block><i class="material-icons">location_on</i>&nbsp;&nbsp;&nbsp;На территории</v-btn>
   </v-layout>
 </template>
 
@@ -17,7 +17,7 @@ export default {
       this.$router.push({ name: 'list', params: { id: 57, name: 'Список согласованных пропусков' } })
     },
     inside () {
-      this.$router.push({ name: 'list', params: { id: 53, name: 'Список пропусков на териитории' } })
+      this.$router.push({ name: 'list', params: { id: 53, name: 'Список пропусков на территории' } })
     }
   }
 }
