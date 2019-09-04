c0be18e47326b1d9d8c3906504c1d391e08e686c
nightwelf <bpo@elem.ru>
2019-09-04 11:01:00 +0500

    фикс 3 для нового пропуска
    -
    
    METADATA: v3.r19

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'es6-promise/auto'
 import auth from '@/auth'
 auth.initAuth()
 Vue.prototype.$appName = 'Разовый пропуск'
-Vue.prototype.$appVersion = 'v3.r18'
+Vue.prototype.$appVersion = 'v3.r19'
 try {
   Vue.prototype.$globalUserData = {permissions: {}, kpps: []}
   if (localStorage.getItem('globalUserData') !== 'undefined') {
